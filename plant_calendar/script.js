7e9735d6b4c46ac06d33b6867468bbaa5ba1ae8e
Sandyna <Sandyna@users.noreply.github.com>
2026-02-01 23:52:30 +0100

    Update script.js

--- plant_calendar/script.js
+++ plant_calendar/script.js
@@ -6,6 +6,7 @@ let selectedPlantIds = new Set();
 document.addEventListener('DOMContentLoaded', () => {
   loadPlantData();
   setupFrostDateInput();
+  renderSelectedPlants();
 });
 
 // ---------- LOAD PLANT DATA ----------
@@ -154,33 +155,4 @@ function renderSelectedPlants() {
   });
 }
 
-function renderPlantOptions() {
-  const container = document.getElementById('plant-options');
-  container.innerHTML = '';
 
-  plantData.forEach(plant => {
-    const div = document.createElement('div');
-
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = `plant-${plant.id}`;
-    checkbox.checked = selectedPlantIds.has(plant.id);
-    checkbox.addEventListener('change', () => {
-      if (checkbox.checked) {
-        selectedPlantIds.add(plant.id);
-      } else {
-        selectedPlantIds.delete(plant.id);
-      }
-      renderSelectedPlants();
-      generateYearCalendar(frostDate.getFullYear(), plantData, frostDate);
-    });
-
-    const label = document.createElement('label');
-    label.htmlFor = `plant-${plant.id}`;
-    label.textContent = plant.name;
-
-    div.appendChild(checkbox);
-    div.appendChild(label);
-    container.appendChild(div);
-  });
-}

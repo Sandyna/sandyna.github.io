6289c47d61581f977aa76342f8733e1452848ba1
Sandyna <Sandyna@users.noreply.github.com>
2026-02-22 18:37:37 +0100

    Update json_from_csv.R
    creating ids from id and name. Do not escape backslash.

--- plant_calendar/json_from_csv.R
+++ plant_calendar/json_from_csv.R
@@ -7,6 +7,9 @@ output_file <- "plant_calendar/plants.json"
 df[] <- lapply(df, function(x) {
   ifelse(is.na(x), "null", x)
 })
+#remove special characters and merge id and name to create a new id
+clean_name <- gsub("[^A-Za-z0-9_]", "", df$name)
+df$id <- paste0(df$id, "_", clean_name)
 
 #this clears the file, append in the next ones
 cat("[\n", file = output_file)
@@ -14,15 +17,21 @@ for (i in 1:nrow(df)) {
   row <- df[i, ]
   
   format_val <- function(x) {
-    if (x == "null") return("null")       		# keep null as literal
-      paste0("\"", gsub('"', '\\"', x), "\"")   # wrap everything else in quotes, escape " characters
-  }
-  
+	  # Keep literal null as null
+	  if (is.na(x) || x == "null") return("null")
+	  
+	  # If numeric, return as-is (no quotes)
+	  if (is.numeric(x) || grepl("^-?[0-9]+(\\.[0-9]+)?$", x)) return(x)
+	  
+	  # Otherwise, treat as string: escape quotes and backslashes
+	  x <- gsub("\"", "\\\\\"", x)      # escape double quotes
+	  paste0("\"", x, "\"")
+	}
   cat("{\n",
       "\t\"id\": ", format_val(row$id), ",\n",
       "\t\"name\": ", format_val(row$name), ",\n",
-      "\t\"sow_indoors\": ", format_val(row$sow_indoors), ",\n",
-      "\t\"sow_outdoors\": ", format_val(row$sow_outdoors), ",\n",
+      "\t\"sow_indoor\": ", format_val(row$sow_indoors), ",\n",
+      "\t\"sow_outdoor\": ", format_val(row$sow_outdoors), ",\n",
       "\t\"transplant\": ", format_val(row$transplant), ",\n",
       "\t\"sun_needs\": ", format_val(row$sun_needs), ",\n",
       "\t\"water_needs\": ", format_val(row$water_needs), ",\n",
